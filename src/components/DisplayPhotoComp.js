3caac1027b9febd135e9f0e9faad541aba03cfd6
MarguoNana <n.marguerite@live.fr>
2019-02-12 22:22:14 +0100

    CapThira-550 : Integration dynamique des img

--- src/components/DisplayPhotoComp.js
+++ src/components/DisplayPhotoComp.js
@@ -1,35 +1,56 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 class DisplayPhotoComp extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            img: [
+                {title:"Ma premiere image"},
+                {title: "Ma seconde image"},
+                {title: "Third img"}
+            ]
+        }
+    }
 
-    render(){
-        return(
-            <div className="main-posts">
-                <div className="container">
-                    <div className="row">
-                        <div className="blog-masonry masonry-true isotope" >
-                            <div className="post-masonry col-md-4 col-sm-6 isotope-item" >
-                                <div className="post-thumb">
-                                    <img src="https://www.freeiconspng.com/uploads/instagram-photo-camera-logo-outline-icons--free-download-14.jpg" alt="" />
-                                    <div className="titre-image">
-                                        <h4><a href="#">Title one goes here</a></h4>
-                                    </div>
-                                    <div className="post-hover text-center">
-                                        <div className="inside">
-                                            <i className="fa fa-plus"></i>
-                                            <span className="date">25 Jan 2084</span>
-                                            <h4><a href="#">Title one goes here</a></h4>
-                                            <p>Cum sociis natoque penatibus et magnis dis parturient</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> 
+    render = function(){
+            var colImage = this.state.img.map(function(el){
+                return (
+                <Col  style={{height:200 +'px', weight:200 + 'px'}} >
+                    <img src="https://www.freeiconspng.com/uploads/instagram-photo-camera-logo-outline-icons--free-download-14.jpg" alt="" style={{height:160 +'px', weight:160 + 'px'}}/>
+                    <div className="titre-image">
+                        <h4><i href="#">{el.title}</i></h4>
+                    </div>
+                    <div className="post-hover text-center">
+                        <div className="inside">
+                            <i className="fa fa-plus"></i>
+                            <span className="date">25 Jan 2084</span>
+                            <h4><a href="#">Title one goes here</a></h4>
+                            <p>Cum sociis natoque penatibus et magnis dis parturient</p>
                         </div>
                     </div>
+                </Col>
+                )
+            });
+    
+            console.log(colImage);
+    
+            return(
+                <div className="main-posts">
+                    <Container>
+                        <Row>{colImage}
+                           
+                        </Row>
+                        <Row>
+    
+                        </Row>
+                    </Container>
                 </div>
-            </div>
-        )
-    }
+            )
+        
+
+        }
+    
 }
 
 export default DisplayPhotoComp;
