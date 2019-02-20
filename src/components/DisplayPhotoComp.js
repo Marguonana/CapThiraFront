a2075026d9fff02e5b55e6aafb94393fc1fba5a5
MarguoNana <n.marguerite@live.fr>
2019-02-20 01:40:14 +0100

    Commit provisoire en attendant les cartes. Suppression OK, ajout à faire

--- src/components/DisplayPhotoComp.js
+++ src/components/DisplayPhotoComp.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import Radium from 'radium';
 require('underscore');
+
 import { Container, Row, Col } from 'reactstrap';
+
+
 
 class DisplayPhotoComp extends React.Component{
     constructor(props){
@@ -34,12 +38,57 @@ class DisplayPhotoComp extends React.Component{
                 {title: "Enième img",img:{
                     data: "http://www.rangerwoodperiyar.com/images/joomlart/demo/default.jpg"
                 }}
-            ]
-        }
+            ],
+            selectedFile: null
+        };
+        
+        
     }
 
+    
+
+    getStyles() {
+        return {
+          deleteButton: {
+            position: "absolute",
+            right: "10px",
+            color: 'black',
+            ":hover": {
+              color: 'red',
+              cursor: "pointer"
+            }
+          },
+          colStyle: {
+            height: 'auto',
+            width: '20%',
+            maxWidth: '20%',
+            marginRight: '5%'
+
+            },
+            imageStyle : {
+                height: '150px',
+                width: '150px'
+            },
+            mainPost : {
+                marginTop: '2%',
+                paddingTop: '10px',
+                height: '58%',
+                width: '95%',
+                backgroundColor: '#F2F2F1',
+                boxShadow: '5px -2px 5px 5px #eee'
+
+
+            },
+            ajouterPhoto : {
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                marginTop: '3%'
+            }
+        };
+      }
+
     encodeImg(res){
-        console.log(res)
+        // console.log(res)
         for(var i = 0; i < res.length; i++){
             if (res[i].img.data){
                 var prefix = "data:image/jpeg;base64,"; // Prefix par default
@@ -58,13 +107,13 @@ class DisplayPhotoComp extends React.Component{
             }
         } 
         this.setState({album: res});
-        console.log(this.state);
+        // console.log(this.state);
         
     }
 
     getPhoto(){
         var myHeaders = new Headers();
-        fetch(new Request('http://localhost:3000/', {
+        fetch(new Request('http://localhost:3000/get', {
             method: 'GET',
             cache: 'default'
           }),myHeaders)
@@ -77,38 +126,61 @@ class DisplayPhotoComp extends React.Component{
     componentDidMount(){
         this.getPhoto();        
     }
+
+    handleClick = (e, el) => {
+          const requestOptions = {
+            method: 'DELETE'
+          };
+        
+          fetch("http://localhost:3000/delete/" + el._id, requestOptions).then((response) => {
+            return response.json();
+          }).then((result) => {
+            console.log(result);
+          }).catch((error) => {
+            console.error('Erreur d\'envoie des params');
+          
+          });
+      }
+
+      handleSelectedFile = event => {
+        this.setState({
+          selectedFile: event.target.files[0],
+          loaded: 0,
+        })
+        console.log(event.target.file[0].name)
+      }
+
+    
     
     render = function(){
-            console.log(this.state.album)
-            var colImage = this.state.album.map(function(el){
-                return (
-                <Col  style={{height:'auto', weight:20 + '%'}} >
-                    <img src={el.img.data} alt="" style={{height:150+'px', weight:150 + 'px'}}/>
+       
+            const styles = this.getStyles();
+            var colImage = this.state.album.map( (el,index) => {
+                return ( 
+                    <Col key={index} style={styles.colStyle} >
+                    <img key={index + '_img'} src={el.img.data} alt="" style={styles.imageStyle}/>
                     <div className="titre-image">
-                        <h4><i href="#">{el.titre}</i></h4>
+                        <h4>{el.titre}<i key={'delete_' + index} onClick={(e) => this.handleClick(e, el)} className="fa fa-times" style={styles.deleteButton}></i></h4>
                     </div>
                     <div className="post-hover text-center">
                         <div className="inside">
-                            <i className="fa fa-plus"></i>
                             <span className="date">{el.datePublication}</span>
-                            <h4><a href="#">Titre d'un commentaire?</a></h4>
-                            <p>Cum sociis natoque penatibus et magnis dis parturient</p>
                         </div>
                     </div>
                 </Col>
                 )
             });
     
-            console.log(colImage);
-    
             return(
-                <div className="main-posts">
+                <div className="main-posts" style={styles.mainPost}>
                     <Container>
                         <Row>{colImage}
                            
                         </Row>
-                        <Row>
-    
+                        <Row><form method="post" encType="multipart/form-data" action="http//localhost:3000/post" style={styles.ajouterPhoto}>
+                                <input type='file' accept='image/png,image/jpeg' name='ajouter' onChange={this.handleSelectedFile}></input>
+                                <input type="submit" value="Valider" />
+                            </form>
                         </Row>
                     </Container>
                 </div>
@@ -118,5 +190,6 @@ class DisplayPhotoComp extends React.Component{
         }
     
 }
+{/* <i className="fa fa-plus"></i> */}
 
-export default DisplayPhotoComp;
+export default Radium(DisplayPhotoComp);
