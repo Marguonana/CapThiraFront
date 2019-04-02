78335557a28556767925bd14b813b14c95e50386
MarguoNana <n.marguerite@live.fr>
2019-04-02 21:45:02 +0200

    CapThira-861: Refactoring css - header de l'album

--- src/components/cover/coverStyle.js
+++ src/components/cover/coverStyle.js
@@ -1,5 +1,26 @@
 export function getStyles() {
     return {
+
+
+
+        header: {
+            height: "54px",
+            boxShadow: "2px 4px 10px #cccccc",
+            color: "#202124",
+            display: "flex",
+            overflow: "hidden",
+            zIndex: "4",
+            width: "100%",
+            background: "#fff",
+            textAlign: "center",
+            fontFamily: "Roboto",
+            paddingLeft: "300px"
+        }
+
+
+
+
+        ,
         bannerStyle : {
             height: '200px',
             width: '100%',
