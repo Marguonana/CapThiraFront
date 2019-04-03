4216b2f2545566b7ff3e969b4b1232d9ec4e49f0
MarguoNana <n.marguerite@live.fr>
2019-04-03 23:16:26 +0200

    CapThira-861: Nouveau css de la page profil

--- src/components/album/albumStyle.js
+++ src/components/album/albumStyle.js
@@ -5,73 +5,78 @@ export function getStyles() {
             display: "flex",
             justifyContent: "space-between"
         },
-      deleteButton: {
-        position: "absolute",
-        color: "#9a9a9a",
-        borderRadius: "5px",
-        padding: "5px",
-        left: "145",
-        ":hover": {
-            transitionProperty: "background-color",
-            transitionDuration: ".5s",
-            transitionTimingFunction: "ease-in",
-            backgroundColor: '#e0e0e0',
-            cursor: "pointer",
-        }
-      },
-      colStyle: {
-        height: 'auto',
-        width: '20%',
-        maxWidth: '20%',
-        minWidth: "150px",
-        minHeight:"150px",
-        marginRight: '5%',
-        marginBottom: '3%'
-
-        },
-        imageStyle : {
-            border: '25px solid #f3f3f3',
-            marginBottom: '4%'
-        },
-        mainPost : {
-            marginTop: '2%',
-            paddingTop: '10px',
-            marginLeft:'auto',
-            marginRight: 'auto',
-            width: '95%',
-
-
-
-        },
-        ajouterPhoto : {
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            marginTop: '3%'
-        },
-        button : {
-            display: 'none'
-        },
-        labelButton : {
-            cursor: 'pointer',
-            width: "260px",
-            height: "42px",
-            margin: "26px 0px 0px",
+        deleteButton: {
+            position: "absolute",
+            color: "#9a9a9a",
+            borderRadius: "5px",
             padding: "5px",
-            border : "none",
-            textAlign: "center",
-            borderRadius : "25px",
-            color : "#fff",
-            backgroundColor :"#007bff",
-            ":hover" : {
-                backgroundColor: "#0062cc"
-
+            left: "145",
+            ":hover": {
+                transitionProperty: "background-color",
+                transitionDuration: ".5s",
+                transitionTimingFunction: "ease-in",
+                backgroundColor: '#e0e0e0',
+                cursor: "pointer",
             }
         },
-        date : {
-            fontSize: '8px',
-            marginBottom: '3%',
-            textAlign: 'right'
-        },
+        colStyle: {
+            height: 'auto',
+            width: '20%',
+            maxWidth: '20%',
+            minWidth: "150px",
+            minHeight:"150px",
+            marginRight: '5%',
+            marginBottom: '3%'
 
-       };
+            },
+            imageStyle : {
+                border: '25px solid #f3f3f3',
+                marginBottom: '4%'
+            },
+            mainPost : {
+                paddingTop: '60px',
+                marginLeft:'300px',
+                marginRight: 'auto',
+                width: '95%',
+               
+            },
+            form: {
+                display: 'inline-block',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                textAlign: 'left'
+            },
+            ajouterPhoto : {
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                paddingTop: '50px',
+                display: 'block',
+                textAlign: 'center'
+            },
+            button : {
+                display: 'none'
+            },
+            labelButton : {
+                cursor: 'pointer',
+                width: "260px",
+                height: "42px",
+                margin: "26px 0px 0px",
+                padding: "5px",
+                border : "none",
+                textAlign: "center",
+                borderRadius : "25px",
+                color : "#fff",
+                backgroundColor :"#007bff",
+                ":hover" : {
+                    backgroundColor: "#0062cc"
+
+                }
+            },
+            date : {
+                fontSize: '8px',
+                marginBottom: '3%',
+                textAlign: 'right'
+            },
+
+        };
   }
